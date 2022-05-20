ddb24bc69020ec3570d6faa941cbb319c789810d
fannyibanez <fannyibanez89@gmail.com>
2022-05-20 10:19:06 +0200

    client model created

---
+++ db/migrate/20220520081714_create_clients.rb
@@ -0,0 +1,18 @@
+class CreateClients < ActiveRecord::Migration[6.1]
+  def change
+    create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone_number
+      t.string :department
+      t.integer :age
+      t.float :revenu
+      t.boolean :advisors_interested
+      t.string :study_level
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
