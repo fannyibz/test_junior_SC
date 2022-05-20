eb5e8b8a72d0ccc57ea51e58377af690820cf5bc
fannyibanez <fannyibanez89@gmail.com>
2022-05-20 10:31:24 +0200

    model client: migration + validation

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,13 @@
 class Client < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :age, presence: true, numericality: true
+  validates :revenu, presence: true, numericality: true
+  validates :email, format: { with: /\A(.+)@(.+)\z/, message: "Email invalid"  },
+                    uniqueness: { case_sensitive: false },
+                    presence: true,
+                    length: { minimum: 4, maximum: 254 }
 end
