412dc0360536852fb58efed4e4751b1e7490b694
fannyibanez <fannyibanez89@gmail.com>
2022-05-23 12:26:43 +0200

    client: controller - views - routes

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,40 @@
+class ClientsController < ApplicationController
+  before_action :set_client, only: %i[ edit update ]
+
+  def index
+    @clients = Client.all
+  end
+
+  def edit
+  end
+
+  def update
+    p @client
+    p '_____________________---------'
+
+    if @client.update(client_params)
+      p @client
+      binding.pry
+      @client.done!
+      redirect_to clients_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_client
+    @client = Client.find(params[:id])
+  end
+
+
+  def client_params
+    params.require(:client).permit(:id, :first_name, :first_name, :email, :phone_number, :department, :age, :revenue, :advisors_interested, :study_level)
+  end
+
+end
+
+
+   
+
